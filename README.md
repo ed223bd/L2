737c6ffeec8dfd1c8540fef2dfe14633a3ed19bf
Ebba Dahlberg <ed223bd@student.lnu.se>
2025-09-20 16:45:43 +0200

    Creates the correct number of bars

---
+++ README.md
@@ -0,0 +1,13 @@
+### Graph maker
+
+This is a module that will generate graphs based on JSON data from the user. 
+
+## Features
+
+- Creates an image that shows the data in graphs.
+- Cusomizeable font and colors in the graphs.
+
+## Technologies
+
+- Vanilla JavaScript
+- Node.js
--- src/js/BarGraphManager.js
+++ src/js/BarGraphManager.js
@@ -1,40 +1,54 @@
 export class BarGraphManager {
-  // TODO: remove. Just to test the creation of graphs
-  // Should come from statisticsManager as "label"
-  // and "value" (standard for all graphs)
-  data = [
-    { city: "Stockholm", rainAmount: "21" },
-    { city: "Sundsvall", rainAmount: "23" },
-    { city: "Karlstad", rainAmount: "18" }
-  ]
 
-  createBar() {
+  // IMORGON
+  // Map data to "label" and "value"
+
+  // Right now the graphs are static, make them 
+  // dynamic using the data at the top of the file
+  // Set svgHeight and svgWidth
+  // Do some loop (forEach?) to create multiple 
+  // bars, dsepending on data.length ??
+
+  // Move formatting to StatisticsManager
+
+  // Should take an array of objects as parameter
+  createBar(data) {
     const svgWidth = 450
     const svgHeight = 300
     const svg = document.querySelector('svg')
 
-    const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+    data.forEach((d, i) => {
+      const x = i * 2 * 30 + 10
 
-    rect.setAttribute('x', 50)
-    rect.setAttribute('y', 70)
-    rect.setAttribute('height', 100)
-    rect.setAttribute('width', 30)
-    rect.setAttribute('fill', 'blue')
-    rect.setAttribute('fill-opacity', 0.2)
+      const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
 
-    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+      rect.setAttribute('x', x)
+      rect.setAttribute('y', 70)
+      rect.setAttribute('height', 100)
+      rect.setAttribute('width', 30)
+      rect.setAttribute('fill', 'blue')
+      rect.setAttribute('fill-opacity', 0.2)
 
-    text.setAttribute('x', 30)
-    text.setAttribute('y', 90)
-    text.textContent = 'Hej'
+      const text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
 
-    // Add new element/s to SVG
-    svg.appendChild(rect)
-    svg.appendChild(text)
+      text.setAttribute('x', x)
+      text.setAttribute('y', 90)
+      text.textContent = 'Hej'
+
+      // Add new element/s to SVG
+      svg.appendChild(rect)
+      svg.appendChild(text)
+    })
+
   }
 }
 
 // Calls the createBar method, to enable
 // viewing in browser
 const barGraphManager = new BarGraphManager()
-barGraphManager.createBar()
+const data = [
+  { label: "Stockholm", value: "21" },
+  { label: "Sundsvall", value: "23" },
+  { label: "Karlstad", value: "18" }
+]
+barGraphManager.createBar(data)
