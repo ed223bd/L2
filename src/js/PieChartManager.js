8af248089cab00ba3b8c70294cefc738a760be6b
Ebba Dahlberg <ed223bd@student.lnu.se>
2025-09-24 00:54:12 +0200

    Attempt at reconstruction of PieChartManager

--- src/js/PieChartManager.js
+++ src/js/PieChartManager.js
@@ -1,10 +1,21 @@
 export class PieChartManager {
-  createPieChart(data) {
-    const svg = document.querySelector('#pieChart')
+  
+  constructor(svgId, width, height) {
+    this.svg = document.querySelector(`${svgId}`)
+    this.svgWidth = width
+    this.svgHeight = height
+    this.radius = 100 // Hard coded for now
+  }
 
-    const svgWidth = 450
-    const svgHeight = 300
-
+  /**
+   * Public method that is the starting point for drawing the pie chart.
+   * 
+   * @param {Array} data - An array of objects, set in the app.
+   * @param {HTMLElement} svg - The element to draw to.
+   * @param {number} svgWidth - The width of the svg element.
+   * @param {number} svgHeight - The height of the svg element.
+   */
+  createPieChart(data, svg, svgWidth, svgHeight, radius) {
     let sum = 0
     data.forEach(d => {
       sum += parseInt(d.value)
@@ -12,78 +23,85 @@ export class PieChartManager {
     console.log(sum)
 
     let startAngle = 0
-    const radius = 110
+
+    const xMiddle = this.svgWidth / 2
+    const yMiddle = this.svgHeight / 2
 
     data.forEach(d => {
+      const label = d.label
       const slice = d.value / sum
       // console.log(slice)
 
       const sliceAngle = slice * 2 * Math.PI
       console.log(sliceAngle)
 
-      const xMiddle = svgWidth / 2
-      const yMiddle = svgHeight / 2
+      // Calls on private method to do the drawing
+      this.#createSlice(xMiddle, yMiddle, startAngle, sliceAngle)
 
-      const xStart = xMiddle + radius * Math.cos(startAngle)
-      const yStart = yMiddle + radius * Math.sin(startAngle)
-      const xEnd = xMiddle + radius * Math.cos(startAngle + sliceAngle)
-      const yEnd = yMiddle + radius * Math.sin(startAngle + sliceAngle)
-
-      let largeArcFlag
-      if (sliceAngle > Math.PI) {
-        largeArcFlag = 1
-      } else {
-        largeArcFlag = 0
-      }
-
-      const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-
-      // TODO: remove. Hard coded for static graphics
-      // path.setAttribute('d', `
-      //   M 100 100
-      //   L 150 100
-      //   A radius radius 0 0 1 100 150
-      //   Z
-      // `)
-      path.setAttribute('d', `
-        M ${xMiddle} ${yMiddle}
-        L ${xStart} ${yStart}
-        A ${radius} ${radius} 0 ${largeArcFlag} 1 ${xEnd} ${yEnd}
-        Z 
-      `
-      )
-
-      path.setAttribute('fill', 'green')
-      path.setAttribute('fill-opacity', 0.5)
-      path.setAttribute('stroke', 'black')
-      path.setAttribute('stroke-width', 2)
-      svg.appendChild(path)
-
-      // Dela upp i create bars 
-      // Nedan ska vara i create labels
-
-      const middleAngle = startAngle + sliceAngle / 2
-
-      const xLabel = xMiddle + radius * 1.2 * Math.cos(middleAngle)
-      const yLabel = yMiddle + radius * 1.2 * Math.sin(middleAngle)
-
-
-      const text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
-
-      if (Math.cos(middleAngle) > 0) {
-        text.setAttribute('text-anchor', 'start')
-      } else {
-        text.setAttribute('text-anchor', 'end')
-      }
-      text.setAttribute('x', xLabel)
-      text.setAttribute('y', yLabel)
-      text.textContent = d.label
-
-      svg.appendChild(text)
-
-      startAngle += sliceAngle
-
+      // Calls on private method to do the drawing
+      this.#createLabel(xMiddle, yMiddle, startAngle, sliceAngle, label)
     })
+  }
+
+  #createSlice(xMiddle, yMiddle, startAngle, sliceAngle, radius) {
+    const xStart = xMiddle + radius * Math.cos(startAngle)
+    const yStart = yMiddle + radius * Math.sin(startAngle)
+    const xEnd = xMiddle + radius * Math.cos(startAngle + sliceAngle)
+    const yEnd = yMiddle + radius * Math.sin(startAngle + sliceAngle)
+
+    let largeArcFlag
+    if (sliceAngle > Math.PI) {
+      largeArcFlag = 1
+    } else {
+      largeArcFlag = 0
+    }
+
+    const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+
+    // TODO: remove. Hard coded for static graphics
+    // path.setAttribute('d', `
+    //   M 100 100
+    //   L 150 100
+    //   A radius radius 0 0 1 100 150
+    //   Z
+    // `)
+    path.setAttribute('d', `
+      M ${xMiddle} ${yMiddle}
+      L ${xStart} ${yStart}
+      A ${radius} ${radius} 0 ${largeArcFlag} 1 ${xEnd} ${yEnd}
+      Z 
+    `
+    )
+
+    path.setAttribute('fill', 'green')
+    path.setAttribute('fill-opacity', 0.5)
+    path.setAttribute('stroke', 'black')
+    path.setAttribute('stroke-width', 2)
+    this.svg.appendChild(path)
+  }
+
+  #createLabel() {
+
+    const middleAngle = startAngle + sliceAngle / 2
+
+    const xLabel = xMiddle + radius * 1.2 * Math.cos(middleAngle)
+    const yLabel = yMiddle + radius * 1.2 * Math.sin(middleAngle)
+
+
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
+
+    if (Math.cos(middleAngle) > 0) {
+      text.setAttribute('text-anchor', 'start')
+    } else {
+      text.setAttribute('text-anchor', 'end')
+    }
+    text.setAttribute('x', xLabel)
+    text.setAttribute('y', yLabel)
+    text.textContent = d.label
+
+    this.svg.appendChild(text)
+
+    startAngle += sliceAngle
   }
 }
 
