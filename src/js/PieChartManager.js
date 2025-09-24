b6e8cd53a3093f1f7a76e220dbbabf87317c0523
Ebba Dahlberg <ed223bd@student.lnu.se>
2025-09-24 07:10:54 +0200

    Setting private fields in PieChartManager

--- src/js/PieChartManager.js
+++ src/js/PieChartManager.js
@@ -1,19 +1,20 @@
 export class PieChartManager {
+  #svg
+  #svgWidth
+  #svgHeight
+  #radius
 
   constructor(svgId, width, height) {
-    this.svg = document.querySelector(`#${svgId}`)
-    this.svgWidth = width
-    this.svgHeight = height
-    this.radius = 100 // Hard coded for now
+    this.#svg = document.querySelector(`#${svgId}`)
+    this.#svgWidth = width
+    this.#svgHeight = height
+    this.#radius = this.#svgHeight / 3 
   }
 
   /**
    * Public method that is the starting point for drawing the pie chart.
    * 
    * @param {Array} data - An array of objects, set in the app.
-   * @param {HTMLElement} svg - The element to draw to.
-   * @param {number} svgWidth - The width of the svg element.
-   * @param {number} svgHeight - The height of the svg element.
    */
   createPieChart(data) {
     let sum = 0
@@ -24,8 +25,8 @@ export class PieChartManager {
 
     let startAngle = 0
 
-    const xMiddle = this.svgWidth / 2
-    const yMiddle = this.svgHeight / 2
+    const xMiddle = this.#svgWidth / 2
+    const yMiddle = this.#svgHeight / 2
 
     data.forEach(d => {
       const label = d.label
@@ -36,21 +37,21 @@ export class PieChartManager {
       console.log(sliceAngle)
 
       // Calls on private method to do the drawing
-      this.#createSlice(xMiddle, yMiddle, startAngle, sliceAngle, this.radius)
+      this.#createSlice(xMiddle, yMiddle, startAngle, sliceAngle)
 
       // Calls on private method to do the drawing
-      this.#createLabel(xMiddle, yMiddle, startAngle, sliceAngle, this.radius, label)
+      this.#createLabel(xMiddle, yMiddle, startAngle, sliceAngle, label)
 
       // When slice and label has been created, update starting position for next slice
       startAngle += sliceAngle
     })
   }
 
-  #createSlice(xMiddle, yMiddle, startAngle, sliceAngle, radius) {
-    const xStart = xMiddle + radius * Math.cos(startAngle)
-    const yStart = yMiddle + radius * Math.sin(startAngle)
-    const xEnd = xMiddle + radius * Math.cos(startAngle + sliceAngle)
-    const yEnd = yMiddle + radius * Math.sin(startAngle + sliceAngle)
+  #createSlice(xMiddle, yMiddle, startAngle, sliceAngle) {
+    const xStart = xMiddle + this.#radius * Math.cos(startAngle)
+    const yStart = yMiddle + this.#radius * Math.sin(startAngle)
+    const xEnd = xMiddle + this.#radius * Math.cos(startAngle + sliceAngle)
+    const yEnd = yMiddle + this.#radius * Math.sin(startAngle + sliceAngle)
 
     let largeArcFlag
     if (sliceAngle > Math.PI) {
@@ -71,7 +72,7 @@ export class PieChartManager {
     path.setAttribute('d', `
       M ${xMiddle} ${yMiddle}
       L ${xStart} ${yStart}
-      A ${radius} ${radius} 0 ${largeArcFlag} 1 ${xEnd} ${yEnd}
+      A ${this.#radius} ${this.#radius} 0 ${largeArcFlag} 1 ${xEnd} ${yEnd}
       Z 
     `
     )
@@ -80,15 +81,15 @@ export class PieChartManager {
     path.setAttribute('fill-opacity', 0.5)
     path.setAttribute('stroke', 'black')
     path.setAttribute('stroke-width', 2)
-    this.svg.appendChild(path)
+    this.#svg.appendChild(path)
   }
 
-  #createLabel(xMiddle, yMiddle, startAngle, sliceAngle, radius, label) {
+  #createLabel(xMiddle, yMiddle, startAngle, sliceAngle, label) {
 
     const middleAngle = startAngle + sliceAngle / 2
 
-    const xLabel = xMiddle + radius * 1.2 * Math.cos(middleAngle)
-    const yLabel = yMiddle + radius * 1.2 * Math.sin(middleAngle)
+    const xLabel = xMiddle + this.#radius * 1.2 * Math.cos(middleAngle)
+    const yLabel = yMiddle + this.#radius * 1.2 * Math.sin(middleAngle)
 
 
     const text = document.createElementNS('http://www.w3.org/2000/svg', 'text')
@@ -102,6 +103,6 @@ export class PieChartManager {
     text.setAttribute('y', yLabel)
     text.textContent = label
 
-    this.svg.appendChild(text)
+    this.#svg.appendChild(text)
   }
 }
