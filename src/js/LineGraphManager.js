7d1a6998448b510bbc4ec5893c01d2b919d7e6b0
Ebba Dahlberg <ed223bd@student.lnu.se>
2025-10-17 00:22:43 +0200

    Line graph prints with correct spacing between points

--- src/js/LineGraphManager.js
+++ src/js/LineGraphManager.js
@@ -15,24 +15,36 @@ export class LineGraphManager extends BaseChart {
   createLineGraph (data, theme) {
     // console.log(data)
 
-    const spaceBetweenPoints = this.svgWidth - this.margin / data.length
+    const spaceBetweenPoints = (this.svgWidth - this.margin) / data.length
     const highestValue = Math.max(...data.map(d => d.value))
+
     this.createAxis(highestValue)
 
-    // For-loop
-    // för varje data-entry ska den öka med 100 (sen dynamiskt)
-    let currentPoint = this.leftMargin
-    for (let i = 0; i < data.length; i++) {
-      console.log(i)
-      currentPoint += 100
+    let startingPointX = this.leftMargin
 
-      data.forEach(d => {
-      this.#drawLine(currentPoint, spaceBetweenPoints, theme)
-    })
+
+    for (let i = 0; i < data.length - 1; i++) {
+      const value = data[i].value
+      const label = data[i].label
+      console.log(value, label)
+
+      const heightOfPoint = Math.floor(value / highestValue * (this.svgHeight - this.topMargin - this.margin))
+      const heightOfNextPoint = Math.floor(data[i + 1].value / highestValue * (this.svgHeight - this.topMargin - this.margin))
+
+      startingPointX += spaceBetweenPoints * i
+
+      const startingPointY = this.svgHeight - heightOfPoint
+      const nextPointY = this.svgHeight - heightOfNextPoint
+
+      console.log(heightOfPoint)
+
+      console.log(startingPointX, startingPointY, spaceBetweenPoints, nextPointY)
+
+      this.#drawLine(startingPointX, startingPointY, spaceBetweenPoints, nextPointY, theme)
     }
   }
 
-  #drawLine(currentPoint, spaceBetweenPoints, theme) {
+  #drawLine(startingPointX, startingPointY, spaceBetweenPoints, nextPointY, theme) {
 
     // path eller flera line?
     // Path: M(moveto), L(lineto), Z(close)
@@ -42,8 +54,8 @@ export class LineGraphManager extends BaseChart {
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
 
     path.setAttribute('d', `
-      M ${currentPoint} 150
-      L ${spaceBetweenPoints} 195
+      M ${startingPointX} ${startingPointY}
+      L ${spaceBetweenPoints} ${nextPointY}
     `)
     path.setAttribute('fill', 'none')
     path.setAttribute('stroke', theme.color)
